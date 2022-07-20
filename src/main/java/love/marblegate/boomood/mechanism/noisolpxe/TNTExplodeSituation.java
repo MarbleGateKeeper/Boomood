58946d18ee7c4be39c37edd60dae3f38f79ecb88
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-07-20 18:49:37 +0800

    WIP #3

---
+++ src/main/java/love/marblegate/boomood/mechanism/noisolpxe/TNTExplodeSituation.java
@@ -0,0 +1,12 @@
+package love.marblegate.boomood.mechanism.noisolpxe;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class TNTExplodeSituation implements Situation{
+    @Override
+    public void revert(Level level, BlockPos blockPos, Player manipulator) {
+
+    }
+}
